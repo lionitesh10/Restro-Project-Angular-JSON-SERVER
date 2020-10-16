05218c78ff1b2fc54b02733d107a116c4f010d36
Nitesh Sapkota <49119262+lionitesh10@users.noreply.github.com>
2020-10-16 20:40:01 +0545

    Added add,update,list,login and register component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddComponent} from './add/add.component';
+import {ListComponent} from './list/list.component';
+import {UpdateComponent} from './update/update.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"add",
+    component:AddComponent
+  },
+  {
+    path:"list",
+    component:ListComponent
+  },
+  {
+    path:"update",
+    component:UpdateComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"register",
+    component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
